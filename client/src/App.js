9c32bf2042470b518f424660242e33fcaa1dac3c
tjethva <tjethva1812@gmail.com>
2024-10-30 19:38:13 +0530

    change

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
             Learn React
           </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Pagess</Link>
+          <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
